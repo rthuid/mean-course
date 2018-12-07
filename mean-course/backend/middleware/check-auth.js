f30e8b586067d2314018a7b92759082de804b50f
Ratheesh <rpadinjareambattu@opentrends.net>
2018-12-07 16:29:02 +0530

    global variables for back end added - env

--- mean-course/backend/middleware/check-auth.js
+++ mean-course/backend/middleware/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedtoken = jwt.verify(token, "secret_this_should_be_longer");
+    const decodedtoken = jwt.verify(token, process.env.JWT_KEY);
     req.userData = {email: decodedtoken.email, userId: decodedtoken.userId }
     next()
   } catch (error) {
